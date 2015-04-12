09ccffd49ced3c2753917170a5b907f5deec0e28
yami12376 <kamil_wit1@wp.pl>
2015-04-12 12:16:10 +0200

    zasada pojedynczej odpowiedzialnosci - SRP

--- APP/Helpers/CounturHelper.cs
+++ APP/Helpers/CounturHelper.cs
@@ -7,25 +7,25 @@ namespace APP.Helpers
 {
     class CounturHelper
     {
-        public Contour LoadBitmap(string patch)
-        {
-            throw new NotImplementedException();
-        }
+        //public Contour LoadBitmap(string patch)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Contour LoadTxt(string patch)
-        {
-            throw new NotImplementedException();
-        }
+        //public Contour LoadTxt(string patch)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Contour SaveBitmap(string patch)
-        {
-            throw new NotImplementedException();
-        }
+        //public Contour SaveBitmap(string patch)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Contour SaveTxt(string patch)
-        {
-            throw new NotImplementedException();
-        }
+        //public Contour SaveTxt(string patch)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
 
         public IQueryable<CounturPoint> GetPointsInsideContur(Contour kontur)
---
+++ APP/Helpers/FileHandling/TxtLoader.cs
@@ -0,0 +1,17 @@
+﻿using APP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Helpers.FileHandling
+{
+    class TxtLoader
+    {
+        public Contour LoadTxt(string patch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
