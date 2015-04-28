ef2e1073be32ac8136eebf2cc1f57761c09a9727
Sylwekqaz <sylwekqaz19@gmail.com>
2015-04-28 19:40:44 +0200

    poprawa

--- APP/Helpers/Pylek.cs
+++ APP/Helpers/Pylek.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace APP.Helpers.FileHandling
+namespace APP.Helpers
 {
     public sealed class Pylek //nie mozna po  niej dziedziczyc
     {
